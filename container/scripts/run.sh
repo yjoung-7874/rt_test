51b3f2332b299f7893ba15cfd4f63c676a9841b1
YJOUNG <61916639+yjoung-7874@users.noreply.github.com>
2026-02-14 01:42:03 +0900

    Update usage instructions and handle CPU list

--- container/scripts/run.sh
+++ container/scripts/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# usage
 print_usage () {
   echo ""
   echo "Usage:"
-  echo "  $0 <engine> <image-name> <dockerfile-path> <result-path> <loop> <test_script>"
-  echo "Examples:"
-  echo "  $0 docker rt_test path/to/dockerfile ~/rt/test/container/results 10000 rt_test.sh "
+  echo "  $0 <engine> <image-name> <dockerfile-path> <result-path> <loop> <test_script> [cpu_list...]"
+  echo "Example:"
+  echo "  $0 docker rt_test path/to/dockerfile ~/rt/results 10000 rt_test.sh 0 1 2 3"
 }
 
 if [ $# -lt 6 ]; then
@@ -21,6 +20,13 @@ RESULT_DIR=$4
 LOOP=$5
 TEST_SCRIPT=$6
 
+shift 6
+CPU_LIST=("$@")
+
 bash ./scripts/env/env_build.sh -e ${ENGINE} ${IMAGE_NAME} ${DOCKERFILE_PATH}
 
-bash ./scripts/test/test.sh ${IMAGE_NAME} ${RESULT_DIR} ${LOOP} ${TEST_SCRIPT} -e ${ENGINE} -cl 0 1 2 3
+if [ ${#CPU_LIST[@]} -gt 0 ]; then
+  bash ./scripts/test/test.sh ${IMAGE_NAME} ${RESULT_DIR} ${LOOP} ${TEST_SCRIPT} -e ${ENGINE} -cl "${CPU_LIST[@]}"
+else
+  echo "cpu list required"
+fi
